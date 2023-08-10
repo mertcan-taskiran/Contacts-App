f9103bb30aada10b5b3e5478a638a4ab2cd96126
Mertcan Taşkıran <65121494+mertcan-taskiran@users.noreply.github.com>
2023-08-10 11:59:28 +0300

    index js dosyaları eklendi

---
+++ src/components/Contacts/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import List from "./List/index"
+import Form from "./Form/index"
+
+function Contacts() {
+  return (
+    <div>
+        <List />
+        <Form />
+    </div>
+  );
+}
+
+export default Contacts;
