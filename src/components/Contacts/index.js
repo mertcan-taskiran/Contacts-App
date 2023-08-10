cd8c4b47aaee68428a066b10c1f76424181d1b09
Mertcan Taşkıran <65121494+mertcan-taskiran@users.noreply.github.com>
2023-08-10 13:10:19 +0300

    input a veri ekleme

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 import List from "./List/index"
 import Form from "./Form/index"
 
 function Contacts() {
-  return (
-    <div>
-        <List />
-        <Form />
-    </div>
-  );
+
+    const [contacts, setContacts] = useState([]);
+
+    useEffect(() => {
+        console.log(contacts);
+    }, [contacts])
+
+    return (
+        <div>
+            <List />
+            <Form addContact={setContacts} contacts={contacts} />
+        </div>
+    );
 }
 
 export default Contacts;
